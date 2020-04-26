94941aac06e2f372f26878298cff9ef44a46e326
Topher L <topher@meanservers.com>
2020-04-26 18:35:42 +0200

    back to tenant

--- src/Rememberable.php
+++ src/Rememberable.php
@@ -29,8 +29,8 @@ trait Rememberable
 
         //if (isset($this->rememberCachePrefix)) {
         //ToDo: see if using server_hostname or whatever speeds this up
-            $tenant = $_SERVER['SERVER_NAME'];
-            $builder->prefix($tenant);
+            $tenant = app(\Hyn\Tenancy\Environment::class)->tenant();
+            $builder->prefix($tenant->id);
         //}
 
         if (isset($this->rememberCacheDriver)) {
